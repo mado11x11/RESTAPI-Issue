0e2633fa97aa35ef3bd3fe5c3bd5c1dbf843640a
mado11x11 <depass.25u17@gmail.com>
2023-06-11 01:15:19 +0900

    ホスト部分の動的生成

--- README.md
+++ README.md
@@ -20,7 +20,8 @@ Spring Boot: 3.0.4
 
 ---
 ## 1-1 Getリクエストの値が返る
-![1-1](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_1.png)
+![1-1_1](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_1_1.png)
+![1-1_2](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_1_2.png)
 
 ## 1-2 バリデーションについて調べてnameが20文字以上の場合はエラーとする
 ![1-2](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_2.png)
@@ -28,13 +29,16 @@ Spring Boot: 3.0.4
 ## 1-3 バリデーションについて調べてnameが空文字の場合はエラーとする
 ![1-3](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_3.png)
 
-## 2-1 Postが正常に行える　クエリ文字列を受け取る
-![2-1_1](https://github.com/mado11x11/image_wiki/blob/Issue_7/2_1_1.png)
+## 1_4 Postが正常に行える　クエリ文字列を受け取る
+![1_4_1](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_4_1.png)
+![1_4_2](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_4_2.png)
 
-![2-1_2](https://github.com/mado11x11/image_wiki/blob/Issue_7/2_1_2.png)
+## 1-5 Patchが正常に行える
+![1-5](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_5.png)
 
-## 2-2 Pathが正常に行える
-![2-2](https://github.com/mado11x11/image_wiki/blob/Issue_7/2_2.png)
+## 1-6 Deleteが正常に行える
+![1-6](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_6.png)
 
-## 2-3 Deleteが正常に行える
-![2-3](https://github.com/mado11x11/image_wiki/blob/Issue_7/2_3.png)
+## 1-7例外処理
+![1-7_1](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_7_1.png)
+![1-7_2](https://github.com/mado11x11/image_wiki/blob/Issue_7/1_7_2.png)
--- src/main/java/com/example/restapi/NameController.java
+++ src/main/java/com/example/restapi/NameController.java
@@ -2,11 +2,11 @@ package com.example.restapi;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,12 +16,13 @@ import java.util.Map;
 public class NameController {
 
     @GetMapping("/names")
-    public  String getNames(@Size(min=1,max=20,message="1文字以上20文字以内で入力してください")
-                                 @NotBlank(message = "名前を入力してください") @RequestParam("name") String name,
-                                 @RequestParam("birthday") String birthday){
-        return "result: 名前" + name + "生年月日" + birthday;
+    public ResponseEntity<Map<String, String>> getNames(@Size(min = 1, max = 20, message = "1文字以上20文字以内で入力してください")
+                                                            @NotBlank(message = "名前を入力してください")
+                                                            @RequestParam("name") String name, @RequestParam("birthday") String birthday) {
+        return ResponseEntity.ok(Map.of("message", name + ":" + birthday));
     }
 
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST , value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
     public Map<String, String> Exceptions(Exception ex) {
         Map<String, String> errors = new HashMap<>();
@@ -30,21 +31,21 @@ public class NameController {
     }
 
     @PostMapping("/names")
-    public ResponseEntity<String> create(@RequestBody CreateForm create){
-        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
-                .queryParam("name","tanaka")
+    public ResponseEntity<String> create(@RequestBody CreateForm create, UriComponentsBuilder uriComponentsBuilder) {
+        URI url = uriComponentsBuilder
+                .queryParam("name", "tanaka")
                 .build()
                 .toUri();
         return ResponseEntity.created(url).body("name successfully created");
     }
 
     @PatchMapping("/names/{id}")
-    public ResponseEntity<Map<String,String>> update(@PathVariable("id") int id , @RequestBody UpdateForm form){
-        return ResponseEntity.ok(Map.of("message","name successfully updated"));
+    public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id, @RequestBody UpdateForm form) {
+        return ResponseEntity.ok(Map.of("message", "name successfully updated"));
     }
 
     @DeleteMapping("/names/{id}")
-    public ResponseEntity<Map<String,String>> delete(@PathVariable("id") int id){
-        return ResponseEntity.ok(Map.of("message","name successfully delete"));
+    public ResponseEntity<Map<String, String>> delete(@PathVariable("id") int id) {
+        return ResponseEntity.ok(Map.of("message", "name successfully delete"));
     }
 }
